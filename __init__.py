1e1c7c0445f9411fa9957d19826f45419fa9302a
Muhammad Hasif <mhasif175@gmail.com>
2023-01-05 21:15:10 +0800

    sss

--- __init__.py
+++ __init__.py
@@ -4,6 +4,8 @@ from flask import request
 from flask import redirect
 from flask import url_for
 from Forms import CreateUserFrom
+import shelve
+import User
 
 app = Flask(__name__)
 
@@ -17,9 +19,25 @@ def home():
 def register_user():
     user_register = CreateUserFrom(request.form)
     if request.method == 'POST' and user_register.validate():
-        return redirect(url_for('output'))
-    else:
-        return render_template('user_registration.html', form=user_register)
+        users_dict = {}
+        db = shelve.open('user.db', 'c')
+
+        try:
+            users_dict = db['Users']
+        except IOError:
+            print("Error in retrieving Users from user.db.")
+
+        user = User.User(user_register.first_name.data, user_register.last_name.data, user_register.today_date.data, user_register.age.data, user_register.phone_no.data, user_register.gender.data, user_register.email_address.data, user_register.postal_code.data)
+        users_dict[user.get_user_id()] = user
+        db['Users'] = users_dict
+
+        # test message
+        users_dict = db['Users']
+        user = users_dict[user.get_user_id()]
+        print(user.get_first_name(), user.get_last_name(), "was stored in user.db successfully with user_id ==", user.get_user_id())
+        db.close()
+        return redirect(url_for('home'))
+    return render_template('user_registration.html', form=user_register)
 
 
 if __name__ == '__main__':
